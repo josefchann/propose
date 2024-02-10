fb0f2415205da89f3b28862c1fa1ac82c348c2c1
josefchann <hfyjc17@nottingham.edu.my>
2024-02-10 23:44:36 +0800

    finalized

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,176 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import ProposeForm from '../components/ProposeForm';
-import Image from '../static/images/hero.svg';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import GenQoutes from '../components/GenQoutes';
+import { images } from '../data/preload-image-list.json';
+import { propose, qoutes } from '../data/quotes.json';
+import { genRandom } from '../utills';
 
-const Home = ({ className = '' }) => (
-    <header className={`hero ${className}`}>
-        <Container>
-            <Row className="hero_content justify-content-between py-5">
-                <Col md={7} lg={6} className="hero_text-wrapper">
-                    <div className="hero_text">
-                        <h1 className="hero_text-title mb-2">
-                            Afraid To <font className="text-danger">Propose</font> Someone?
-                        </h1>
-                        <p className="hero_text-description">
-                            don’t worry I am here to help you. enter your valentines name and generate a propose link.
-                        </p>
 
-                        <ProposeForm className="hero_text-form mt-4 mt-md-5" />
-                        <p className="hero_credit">
-                            developed by{' '}
-                            <a
-                                href="https://tonmoydeb.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                @tonmoydeb
-                            </a>
-                        </p>
+const Proposal = ({ className = '' }) => {
+    const { id } = useParams();
+
+    const [texts, setTexts] = useState([]);
+    const [currentText, setCurrentText] = useState({
+        image: '/images/image-1.jpg',
+        subtext: 'It is that time of the year again hohoho',
+    });
+
+    const [showYesPopUp, setShowYesPopUp] = useState(false);
+
+    const [showNoPopUp, setShowNoPopUp] = useState(false);
+
+    const handleYes = () => {
+        setShowYesPopUp(true);
+    };
+
+    const handleNo = () => {
+        setShowNoPopUp(true);
+    }
+    
+
+    // add quotes
+    const addQoutes = () => {
+        const qoute = texts.length >= qoutes.length ? propose : genRandom(qoutes, texts);
+        setCurrentText((prevData) => ({ ...prevData, ...qoute }));
+        setTexts((prevData) => [...prevData, qoute]);
+    };
+
+    // handle click
+    const handleClick = (e) => {
+        const button = e.target;
+        // remove previous shaking effect
+        button.classList.remove('shake');
+        // add quote
+        addQoutes();
+        // add shaking effect
+        button.classList.add('shake');
+        // remove shaking effect
+        setTimeout(() => {
+            button.classList.remove('shake');
+        }, 1000);
+    };
+
+    // effects
+    useEffect(() => {
+        document.title = `Bei, Will You Be My Valentine?`;
+    });
+
+    // preload images
+    useEffect(() => {
+        images.forEach((image) => {
+            const img = new Image();
+            img.src = image;
+        });
+    }, []);
+
+    return (
+        <div
+            className={`proposal ${className}`}
+            style={{
+                '--image': `url(${currentText.image})`,
+            }}
+        >
+            <div className="proposal_media bg-dark d-none d-md-block" />
+            <Container>
+                <Row>
+                    <Col md={6} className="ms-auto">
+                        <div className="proposal_content py-5">
+                            <div className="proposal_header">
+                                <h1 className="proposal_title h4">
+                                    HARLOWWWW <b>BEI</b>
+                                </h1>
+                                <p className="propsal_subtitle">{currentText.subtext}</p>
+                            </div>
+
+                            <GenQoutes texts={texts} className="main-content" />
+
+                            {currentText.id !== 'finished' ? (
+                                <Button variant="danger" onClick={handleClick}>
+                                    {texts.length ? 'Next' : 'Continue'}
+                                </Button>
+                            ) : (
+                                <div>
+                                    <span>
+                                        Will you be my valentines? 
+                                        <br/>
+                                        <br/>
+                                    </span>
+                                    <Button className="yes-btn" variant='danger' onClick={handleYes}>
+                                        Yes
+                                    </Button>
+                                    <Button className="no-btn" variant="danger--primary" onClick={handleNo}>
+                                        No 
+                                    </Button>
+                                    <style jsx>{`
+                                    .yes-btn {
+                                        margin-right: 20px;
+                                    }
+
+                                    .popup {
+                                        position: fixed;
+                                        top: 0;
+                                        left: 0;
+                                        width: 100%;
+                                        height: 100%;
+                                        background-color: rgba(0, 0, 0, 0.5);
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                    }
+                                    .popup-content {
+                                        background-color: white;
+                                        padding: 20px;
+                                        border-radius: 8px;
+                                        display: flex;
+                                        flex-direction: column;
+                                        align-items: center;
+                                    }
+
+                                    .goofy{
+                                        width: 35%;
+                                        height: auto;
+                                    }
+
+                                    .goofy-comment{
+                                        text-align: center;
+                                        font-size: 20px;
+                                        margin-top: 20px;
+                                    }
+
+                                    
+                                    `}
+                                    </style>
+                                </div>
+                            )}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            {showYesPopUp && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <img className="goofy" src="/images/goofy.jpg" />
+                        <p className="goofy-comment">Here's to more goofy moments like this with you hehehe</p>
+                        <Button className="close-btn" onClick={() => setShowYesPopUp(false)}>
+                            Close
+                        </Button>
                     </div>
-                </Col>
-                <Col md={5} className="hero_media-wrapper">
-                    <div className="hero_media">
-                        <img
-                            src={Image}
-                            alt="Afraid To Propose Someone?"
-                            className="hero_media-img img-fluid"
-                        />
+                </div>
+            )}
+            {showNoPopUp && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <img className="goofy" src="/images/sad.jpg" />
+                        <p className="goofy-comment">I know you're just kidding. Please don't make me sad</p>
+                        <Button className="close-btn" onClick={() => setShowNoPopUp(false)}>
+                            Close
+                        </Button>
                     </div>
-                </Col>
-            </Row>
-        </Container>
-    </header>
-);
+                </div>
+            )}
+        </div>
+    );
+};
 
-export default Home;
+export default Proposal;
